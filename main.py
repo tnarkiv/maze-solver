4438bae4fb8689d2d4301e6f21db8f6bba68fcbb
tnarkiv <vikrant2099ad@gmail.com>
2025-03-21 15:22:15 +0000

    Added maze

--- main.py
+++ main.py
@@ -1,37 +1,13 @@
 """Main module of the program"""
 
-import random
-from cell import Cell
-from point import Point
+from maze import Maze
 from window import Window
 
 
 def main():
     """Main function"""
     win = Window(800, 600)
-    a_s, b_s = [], []
-    for i in range(50):
-        a_s.append(Point(random.randint(0, 400), random.randint(0, 300)))
-        b_s.append(Point(a_s[i].x + 150, a_s[i].y + 150))
-    cells = []
-    for i in range(50):
-        cell = Cell(
-            a_s[i].x,
-            b_s[i].x,
-            a_s[i].y,
-            b_s[i].y,
-            win,
-            has_left_wall=random.choice([True, False]),
-            has_right_wall=random.choice([True, False]),
-            has_top_wall=random.choice([True, False]),
-            has_bottom_wall=random.choice([True, False]),
-        )
-        cell.draw()
-        win.redraw()
-        cells.append(cell)
-        if i > 0:
-            cells[i].draw_move(cells[i - 1], random.choice([True, False]))
-            win.redraw()
+    Maze(10, 10, 20, 20, 20, 20, win)
     win.wait_for_close()
 
 
---
+++ maze.py
@@ -0,0 +1,66 @@
+"""Maze module"""
+
+import random
+import time
+from cell import Cell
+
+
+class Maze:
+    """Maze class"""
+
+    def __init__(
+        self,
+        x1,
+        y1,
+        num_rows,
+        num_cols,
+        cell_size_x,
+        cell_size_y,
+        win,
+    ):
+        self.x1 = x1
+        self.y1 = y1
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self.win = win
+        self.cells = []
+        self._create_cells()
+
+    def _create_cells(self):
+        self._cells = [
+            [None for _ in range(self.num_cols)] for _ in range(self.num_rows)
+        ]
+        for i in range(self.num_rows):
+            for j in range(self.num_cols):
+                self._draw_cell(i, j)
+
+    def _draw_cell(self, i, j):
+        x1 = self.x1 + j * self.cell_size_x
+        y1 = self.y1 + i * self.cell_size_y
+        x2 = x1 + self.cell_size_x
+        y2 = y1 + self.cell_size_y
+
+        has_left_wall = random.choice([True, False])
+        has_right_wall = random.choice([True, False])
+        has_top_wall = random.choice([True, False])
+        has_bottom_wall = random.choice([True, False])
+
+        self._cells[i][j] = Cell(
+            x1,
+            x2,
+            y1,
+            y2,
+            self.win,
+            has_left_wall,
+            has_right_wall,
+            has_top_wall,
+            has_bottom_wall,
+        )
+        self._cells[i][j].draw()
+        self._animate()
+
+    def _animate(self):
+        self.win.redraw()
+        time.sleep(0.05)
